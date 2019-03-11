0683f0a7d549198faefb51c9713ec6d2d06582ad
vFeng <z1914007838@outlook.com>
2019-03-11 15:11:15 +0800

    fix-修改业务保存逻辑

--- apps/sztu_api/views/click_api.py
+++ apps/sztu_api/views/click_api.py
@@ -26,12 +26,16 @@ def click_api(request):
             'dom_name': request.GET.get('dom_name', default=''),
             'access_time': access_time,
         }
-        logger.info('数据获取成功:[%s]' % (data,))
-        logger.info('数据开始保存')
+        if data['dom_name'] != '':
 
-        add = Click(**data)
-        add.save()
-        logger.info('数据保存完成')
-        print(data)
+            logger.info('数据获取成功:[%s]' % (data,))
+            logger.info('数据开始保存')
+
+            add = Click(**data)
+            add.save()
+            logger.info('数据保存完成')
+            logger.info(data)
+        else:
+            logger.info('请求有误，未获取到[dom_name]')
 
     return HttpResponse('', content_type="image/png")
